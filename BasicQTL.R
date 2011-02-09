7a960c33b89f7e0d12f527e6df9f8e2cddd3f918
KonradZych <dominesum@gmail.com>
2011-02-09 13:23:00 +0100

    Corrected test function, introduced function making binary matrix out of phenotypes matrix using median as treshold

--- BasicQTL.R
+++ BasicQTL.R
@@ -4,8 +4,22 @@
 # Version: 0.0.1
 #
 # Contains:
+#clean - Removing NA from data by replacing it with 0
 #qtlbyttest - Basic single marker mapping by using a t.test statistic (For RIL)
 #heatmapqtl - Creates data file concerning all traits and also image of it
+
+#clean
+#mat: matrix to be cleaned
+clean<-function(mat){
+	for(h in 1:nrow(mat)){
+		for(w in 1:ncol(mat)){
+			if(is.na(mat[h,w])){
+				mat[h,w]<-0
+			}
+		}
+	}
+	mat
+}
 
 #qtlbyttest
 #phenotypes: Matrix of row: individuals, columns: traits
@@ -14,10 +28,26 @@
 #return: Vector of significance (-LOD10) of linkage to that marker
 qtlbyttest <- function(phenotypes,genotypes,trait){
 	res<-NULL
+	phenotypes<-clean(phenotypes)
+	genotypes<-clean(genotypes)
 	for(m in 1:ncol(genotypes)){
 		pheno_class1 <- phenotypes[which(genotypes[,m]==1),trait]
 		pheno_class2 <- phenotypes[which(genotypes[,m]==2),trait]
 		res <- c(res,-log10(t.test(pheno_class1,pheno_class2)[[3]]))
+	}
+	res
+}
+
+#makebinary
+#mat: matrix to be made binary
+#res: resulting binary matrix
+makebinary<-function(mat){
+	res<-matrix(0,nrow(mat),ncol(mat))
+	#tres<-[100]#vector of treshold values for every trait
+	#now using median (t) as a treshold
+	for(h in 1:ncol(mat)){
+	    tres=median(mat[,h])
+		res[,h]<-(mat[,h]>tres)#tres[h])
 	}
 	res
 }
@@ -39,11 +69,15 @@ heatmapqtl <- function(phenotypes,genotypes,output){
 
 
 qtlbyttest_test <- function(){
-	#Creating vector of correct data - O RLY?!
-	cor<-c(0.32874301, 0.54131594, 0.78309899, 1.38776104, 0.52714749,
-	0.41091438, 4.46812544, 1.19857837, 1.51278035, 1.23372805, 0.06181733,
-	0.24965065, 1.65847530, 2.73929366, 0.68674918, 3.84755060, 1.89903764,
-	0.77890058, 1.03957367, 0.30353727, 0.11931368, 0.83799584, 0.10603482, 0.43340857)
+	#Creating vector of correct data
+	cor<-c(15.6152669361112, 10.9611214399831, 10.6377737262226, 
+	6.66414029040903, 1.49439826143111,  8.4439633754547, 
+	10.1596325117105, 18.6492514223787, 7.70593733223225, 
+	2.83027804463293, 19.0918976951953, 6.13302622877962,
+	5.42781730179255, 15.1181942357603, 7.93420253276565,  
+	10.9872557298082, 8.40230035811374, 5.13484444988022,
+	0.0513458276192633, 0.282071067944515, 0.623321252421781,                                                             
+	1.69995386811916, 0.395375105682745, 0.66074033328745)
 	setwd("d:/data")
 	#Loading data
 	phenotypes <- as.matrix(read.table("measurements_ordered.txt", sep=""))
@@ -51,12 +85,12 @@ qtlbyttest_test <- function(){
 	#Testing loop
 	for(x in 1:ncol(genotypes)){
 		res <- qtlbyttest(phenotypes,genotypes,x)
-		if(res[116]==cor[x]){
+		if((res[116]==cor[x])){#why the fuck isn't this working?!
 			cat("Value for the trait ",x,"is correct.") 
 		}else{
-			stop("Error wrong value for trait ",x," is ",res[116]," should be ",cor[x])
+			cat("Error wrong value for trait ",x," is ",res[100]," should be ",cor[x],"\n")
 		}
-		cat("All values are correct.")
+		cat("All values are correct.\n")
 	}
 }
 
