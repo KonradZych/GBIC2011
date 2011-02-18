86e42647f4ac3cb9ae63fe6925508031654ee74a
KonradZych <dominesum@gmail.com>
2011-02-18 16:21:39 +0100

    Google images function

--- dannysassigment.R
+++ dannysassigment.R
@@ -37,3 +37,30 @@ faculty<-function(number){
 	}
 	cat(res,"\n")
 }
+
+save_image_from_url <- function(image_url,image_file){
+	con <- file(image_file,"wb")
+	writeBin(getBinaryURL(image_url),con,useBytes=TRUE)
+	close(con)
+}
+
+
+r_is_made_for_searching_google <- function(searchterm){
+	#setwd(paste("D:/My_searches/",searchterm,sep=""))
+	website <- getURL(paste("http://www.google.nl/images?q=",searchterm,sep=""))
+	cat("Website is ",nchar(website),"characters at begining.\n")
+	results <- regexpr("http(.+?).jpg",website)
+	cat("First match at ",results[[1]],"\n")
+	i<-1
+	while(results>0){
+		if(attr(results,"match.length")<256){
+			cat(i,"OK!",substr(website,results[[1]],results[[1]]+attr(results,"match.length")-1),"\n")
+			save_image_from_url(substr(website,results[[1]],results[[1]]+attr(results,"match.length")-1),paste("image",i,".jpg",sep=""))
+			website <- substr(website,results[[1]]+attr(results,"match.length"),nchar(website))
+		}else{
+			website <- substr(website,results[[1]]+4,nchar(website))
+		}
+		i<-i+1
+		results <- regexpr("http(.+?).jpg",website)
+	}
+}
